2bff1398708f86be6b3d325f8b47a3a362452332
turakz <zachary.turak@gmail.com>
2021-12-14 19:28:36 -0500

    accidentally pushed day 11 solns to day 12, corrected

--- aoc-2021/k11-day/soln.cpp
+++ aoc-2021/k11-day/soln.cpp
@@ -1,195 +1,252 @@
 #include <iostream>
-#include <stack>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
+#include <queue>
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <limits>
+#include <algorithm>
 
-auto findCorruptionScore(const std::vector<std::string>& sequences) -> std::uint64_t
+auto is_valid_matrix_pos(const std::vector<std::vector<std::uint32_t>>& matrix, std::size_t i, std::size_t j) -> bool
 {
-  const auto symbolPairs = std::unordered_map<char, char>
-  {
-    {'(', ')'},
-    {'[', ']'},
-    {'{', '}'},
-    {'<', '>'},
-  };
-  const auto symbolScore = std::unordered_map<char, std::uint64_t>
-  {
-    {')', 3},
-    {']', 57},
-    {'}', 1197},
-    {'>', 25137}
-  };
-  auto corruptionScore = std::uint64_t{0};
-  for (const auto& sequence : sequences)
-  {
-    auto symbolStack = std::stack<char>{};
-    for (const auto& symbol : sequence)
-    {
-      if (symbolStack.empty() || symbolPairs.find(symbol) != std::end(symbolPairs))
-      {
-        symbolStack.push(symbol);
-      }
-      else if (!symbolStack.empty() && (symbolPairs.find(symbol) == std::end(symbolPairs)))
-      {
-        auto expectedSymbol = symbolPairs.find(symbolStack.top())->second;
-        if (symbol != expectedSymbol)
-        {
-          auto score = symbolScore.find(symbol)->second;
-          corruptionScore += score;
-        }
-        symbolStack.pop();
-      }
-    }
-  }
-  return corruptionScore;
+  return (i >= 0 && i < matrix.size() && j >= 0 && j < matrix[i].size());
 }
 
-auto parseIncompleteSequences(const std::vector<std::string>& sequences) -> std::vector<std::string>
+auto to_integer_matrix(const std::vector<std::string>& input_lines) -> std::vector<std::vector<std::uint32_t>>
 {
-  std::cout << "parsing..." << std::endl;
-  const auto symbolPairs = std::unordered_map<char, char>
+  auto integer_matrix = std::vector<std::vector<std::uint32_t>>{};
+  for (const auto& line : input_lines)
   {
-    {'(', ')'},
-    {'[', ']'},
-    {'{', '}'},
-    {'<', '>'},
-  };
-  auto isCorruptedSequence = std::unordered_map<std::size_t, bool>{};
-  auto sequenceIdx = std::size_t{0};
-  for (const auto& sequence : sequences)
-  {
-    auto symbolStack = std::stack<char>{};
-    for (const auto& symbol : sequence)
+    auto ints = std::vector<std::uint32_t>{};
+    for (const auto& n : line)
     {
-      if (symbolStack.empty() || symbolPairs.find(symbol) != std::end(symbolPairs))
-      {
-        symbolStack.push(symbol);
-      }
-      else if (!symbolStack.empty() && (symbolPairs.find(symbol) == std::end(symbolPairs)))
-      {
-        auto expectedSymbol = symbolPairs.find(symbolStack.top())->second;
-        if (symbol != expectedSymbol)
-        {
-          isCorruptedSequence.insert({sequenceIdx, true});
-        }
-        symbolStack.pop();
-      }
+      ints.push_back(n - '0');
     }
-    ++sequenceIdx;
+    integer_matrix.push_back(std::move(ints));
   }
-  auto incompleteSequences = std::vector<std::string>{};
-  for (std::size_t idx = 0; idx < sequences.size(); ++idx)
+  return integer_matrix;
+}
+auto increment_matrix(std::vector<std::vector<std::uint32_t>>& matrix, const std::uint32_t energy_delta) -> std::vector<std::vector<std::uint32_t>>
+{
+  // increase
+  for (std::size_t i = 0; i < matrix.size(); ++i)
   {
-    auto sequence = sequences[idx];
-    if (isCorruptedSequence.find(idx) == std::end(isCorruptedSequence))
+    for (std::size_t j = 0; j < matrix[i].size(); ++j)
     {
-      incompleteSequences.push_back(sequence);
+      matrix[i][j] += energy_delta;
     }
   }
-  return incompleteSequences;
+  return std::move(matrix);
+}
+auto init_matrix_with_existing(std::vector<std::vector<std::uint32_t>>& matrix) -> std::vector<std::vector<std::uint32_t>>
+{
+  for (std::size_t i = 0; i < matrix.size(); ++i)
+  {
+    for (std::size_t j = 0; j < matrix[i].size(); ++j)
+    {
+      matrix[i][j] = 0;
+    }
+  }
+  return std::move(matrix);
 }
 
-auto findMissingSymbols(const std::vector<std::string>& sequences) -> std::vector<std::string> 
+// BFS on neighbors
+auto bfs_flashpoints(
+    std::vector<std::vector<std::uint32_t>>& energy_matrix, 
+    std::vector<std::vector<std::uint32_t>>& visited, 
+    std::size_t curr_i, std::size_t curr_j) 
+  -> std::uint32_t 
 {
-  auto incompleteSequences = parseIncompleteSequences(sequences);
-  const auto symbolPairs = std::unordered_map<char, char>
+  auto adj_directions = std::vector<std::pair<std::size_t, std::size_t>>
   {
-    {'(', ')'},
-    {'[', ']'},
-    {'{', '}'},
-    {'<', '>'},
+    // down
+    {1, 0},
+    // right
+    {0, 1}, 
+    // up
+    {-1, 0}, 
+    // left
+    {0, -1}, 
+    // lower right diag 
+    {1, 1}, 
+    // upper right diag
+    {-1, 1},
+    // lower left diag
+    {1, -1},
+    // upper left diag, 
+    {-1, -1}
   };
-  auto missingSymbolSequences = std::vector<std::string>{};
-  for (const auto& sequence : incompleteSequences)
+  auto frontier = std::queue<std::pair<std::size_t, std::size_t>>{};
+  frontier.push({curr_i, curr_j});
+  visited[curr_i][curr_j] = 1;
+  auto n_flashes = std::uint32_t{0};
+  while (!frontier.empty())
   {
-    auto symbolStack = std::stack<char>{};
-    std::cout << "analyzing sequence: " << sequence;
-    for (const auto& symbol : sequence)
+    auto node = frontier.front();
+    frontier.pop();
+    auto i = node.first;
+    auto j = node.second;
+    for (const auto& adj : adj_directions)
     {
-      if (symbolStack.empty() || symbolPairs.find(symbol) != std::end(symbolPairs))
+      if (is_valid_matrix_pos(visited, i + adj.first, j + adj.second) && is_valid_matrix_pos(energy_matrix, i + adj.first, j + adj.second))
       {
-        symbolStack.push(symbol);
-      }
-      else if (!symbolStack.empty() && (symbolPairs.find(symbol) == std::end(symbolPairs)))
-      {
-        symbolStack.pop();
+        if(visited[i + adj.first][j + adj.second] == 0) 
+        {
+          ++energy_matrix[i + adj.first][j + adj.second];
+          if (energy_matrix[i + adj.first][j + adj.second] > 9) 
+          {
+            ++n_flashes;
+            energy_matrix[i + adj.first][j + adj.second] = 0;
+            visited[i + adj.first][j + adj.second] = 1;
+            frontier.push({i + adj.first, j + adj.second});
+          }
+        }
       }
     }
-    auto missingSymbols = std::string{""};
-    std::cout << " -> malformed close symbols: ";
-    while (!symbolStack.empty())
+  }
+  return n_flashes;
+}
+
+auto step_through_flashpoints(std::vector<std::vector<std::uint32_t>>& energy_matrix, const std::uint32_t max_steps, const std::uint32_t energy_delta) -> std::uint32_t
+{
+  if (energy_matrix.empty())
+  {
+    std::cerr << "ERROR::step_through_flashpoints(...)::matrix_empty" << std::endl;
+    return std::uint32_t{std::numeric_limits<std::uint32_t>::max()};
+  }
+  std::vector<std::vector<std::uint32_t>> visited{};
+  for (std::size_t i = 0; i < energy_matrix.size(); ++i)
+  {
+    auto v = std::vector<std::uint32_t>{};
+    for (std::size_t j = 0; j < energy_matrix[i].size(); ++j)
     {
-      std::cout << symbolPairs.find(symbolStack.top())->second;
-      missingSymbols += symbolPairs.find(symbolStack.top())->second;
-      symbolStack.pop();
+      v.push_back(std::uint32_t{0});
+    }
+    visited.push_back(v);
+  }
+  auto n_flashpoints = std::uint32_t{0};
+  auto step = std::size_t{0};
+  while (step < max_steps)
+  {
+    
+    ++step;
+    energy_matrix = increment_matrix(energy_matrix, energy_delta);
+    visited = init_matrix_with_existing(visited);
+    // bfs
+    for (std::size_t i = 0; i < energy_matrix.size(); ++i)
+    {
+      for (std::size_t j = 0; j < energy_matrix[i].size(); ++j)
+      {
+        if (energy_matrix[i][j] > 9)
+        {
+          energy_matrix[i][j] = 0;
+          n_flashpoints += (1 + bfs_flashpoints(energy_matrix, visited, i, j));
+        }
+      }
+    }
+  }
+  return n_flashpoints;
+}
+
+auto step_through_flashpoints_until_sync(std::vector<std::vector<std::uint32_t>>& energy_matrix, const std::uint32_t energy_delta) -> std::uint32_t
+{
+  if (energy_matrix.empty())
+  {
+    std::cerr << "ERROR::step_through_flashpoints(...)::matrix_empty" << std::endl;
+    return std::uint32_t{std::numeric_limits<std::uint32_t>::max()};
+  }
+  std::vector<std::vector<std::uint32_t>> visited{};
+  for (std::size_t i = 0; i < energy_matrix.size(); ++i)
+  {
+    auto v = std::vector<std::uint32_t>{};
+    for (std::size_t j = 0; j < energy_matrix[i].size(); ++j)
+    {
+      v.push_back(std::uint32_t{0});
+    }
+    visited.push_back(v);
+  }
+  auto n_flashpoints = std::uint32_t{0};
+  auto step = std::size_t{0};
+  while (true)
+  {
+    
+    ++step;
+    std::cout << "step: " << step << std::endl;
+    energy_matrix = increment_matrix(energy_matrix, energy_delta);
+    visited = init_matrix_with_existing(visited);
+    auto isSoln = std::uint32_t{0};
+    // bfs
+    for (std::size_t i = 0; i < energy_matrix.size(); ++i)
+    {
+      for (std::size_t j = 0; j < energy_matrix[i].size(); ++j)
+      {
+        if (energy_matrix[i][j] > 9)
+        {
+          energy_matrix[i][j] = 0;
+          n_flashpoints += (1 + bfs_flashpoints(energy_matrix, visited, i, j));
+        }
+      }
+    }
+    auto n = std::size_t{energy_matrix.size()};
+    auto m = std::size_t{0};
+    for (const auto& row : energy_matrix)
+    {
+      m = row.size();
+      for (const auto& energy_value : row)
+      {
+        isSoln += (energy_value == 0); 
+        std::cout << energy_value << ' ';
+      }
+      std::cout << std::endl;
     }
     std::cout << std::endl;
-    missingSymbolSequences.push_back(std::move(missingSymbols));
-  }
-  return missingSymbolSequences;
-}
-
-auto findMissingSymbolsCompletionScore(const std::vector<std::string>& sequences) -> std::uint64_t
-{
-  const auto symbolScore = std::unordered_map<char, std::uint64_t>
-  {
-    {')', 1},
-    {']', 2},
-    {'}', 3},
-    {'>', 4}
-  };
-  const auto scoreWeight = std::uint64_t{5};
-  auto missingSymbols = std::vector<std::string>{findMissingSymbols(sequences)};
-  auto scores = std::vector<std::uint64_t>{};
-  for (const auto& sequence : missingSymbols)
-  {
-    auto score = std::uint64_t{0};
-    for (const auto& symbol : sequence)
+    if (isSoln == n*m)
     {
-      score *= scoreWeight;
-      score += symbolScore.find(symbol)->second;
+      std::cout << "soln!" << std::endl;
+      return step;
     }
-    std::cout << "sequence: " << sequence << ", score: " << score << std::endl;
-    scores.push_back(score);
   }
-  std::sort(std::begin(scores), std::end(scores));
-  auto mid = std::size_t{scores.size()/2};
-  return scores[mid];
+  return n_flashpoints;
 }
 
-
-auto solvePuzzle(const std::string& inputFileName) -> void
+auto find_n_flashpoints(const std::vector<std::string>& input_lines) -> uint32_t
 {
-  auto ifs = std::ifstream{inputFileName};
+  std::cout << "generating energy matrix..." << std::endl;
+  auto energy_matrix = to_integer_matrix(input_lines);
+  const auto energy_delta = std::uint32_t{1};
+  const auto max_steps = std::uint32_t{100};
+  return step_through_flashpoints(energy_matrix, max_steps, energy_delta);
+}
+
+auto find_first_step_when_flashpoints_sync(const std::vector<std::string>& input_lines) -> uint32_t
+{
+  std::cout << "generating energy matrix..." << std::endl;
+  auto energy_matrix = to_integer_matrix(input_lines);
+  const auto energy_delta = std::uint32_t{1};
+  return step_through_flashpoints_until_sync(energy_matrix, energy_delta);
+}
+auto solve_puzzle(const std::string& input_file_name) -> void
+{
+  auto ifs = std::ifstream{input_file_name};
   if (!ifs.is_open())
   {
-    std::cerr << "ERROR::solvePuzzle(const std::string&)::FAILED_TO_OPEN_FILE: {" << inputFileName << "}" << std::endl;
+    std::cerr << "ERROR::solve_puzzle(const std::string&)::failed_to_open_file: {" << input_file_name << "}" << std::endl;
     return;
   }
   auto line = std::string{""};
-  auto lines = std::vector<std::string>{};
-  while (std::getline(ifs, line))
-  {
-    std::cout << line << std::endl;
-    lines.push_back(line);
-  }
-  auto soln = std::uint64_t{0};
+  auto input_lines = std::vector<std::string>{};
+  while (std::getline(ifs, line)) input_lines.push_back(line);
   std::cout << "---part 1---" << std::endl;
-  soln = findCorruptionScore(lines);
-  std::cout << "soln: " << soln << std::endl;
+  auto n_flashpoints = find_n_flashpoints(input_lines);
+  std::cout << "soln: " << n_flashpoints << std::endl;
   std::cout << "---part 2---" << std::endl;
-  soln = findMissingSymbolsCompletionScore(lines);
-  std::cout << "soln: " << soln << std::endl;
+  auto step = find_first_step_when_flashpoints_sync(input_lines);
+  std::cout << "soln: " << step << std::endl;
 }
 
 auto main(void) -> int
 {
-  //solvePuzzle("example-input.txt");
-  solvePuzzle("input.txt");
+  //solve_puzzle("example-input.txt");
+  solve_puzzle("input.txt");
   return 0;
 }
-
